199509927d47f0f7506cabe42a963aa9eb55fa61
Zhandos04 <nurbekulyzhandos@gmail.com>
2024-10-16 22:07:39 +0500

    Navbar updated

--- app/src/main/java/com/example/finalproject/ui/Navigation.kt
+++ app/src/main/java/com/example/finalproject/ui/Navigation.kt
@@ -16,7 +16,6 @@ import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.navigation.NavController
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
