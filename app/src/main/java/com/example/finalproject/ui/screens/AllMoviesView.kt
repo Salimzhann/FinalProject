0d2e5fbf7257de5fdbca04e20c8c40bc7a4c37f2
Salimzhann <manassalimzhan@icloud.com>
2024-10-31 20:16:02 +0500

    change some UI components

--- app/src/main/java/com/example/finalproject/ui/screens/AllMoviesView.kt
+++ app/src/main/java/com/example/finalproject/ui/screens/AllMoviesView.kt
@@ -1,5 +1,6 @@
 package com.example.finalproject.ui.screens
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.grid.GridCells
@@ -7,6 +8,7 @@ import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.finalproject.domain.viewmodel.MainPageViewModel
 
@@ -14,11 +16,17 @@ import com.example.finalproject.domain.viewmodel.MainPageViewModel
 fun AllMoviesView(movies: List<MainPageViewModel.MovieItem>) {
     LazyVerticalGrid(
         columns = GridCells.Fixed(2),
-        contentPadding = PaddingValues(10.dp),
-        modifier = Modifier.fillMaxSize()
+        contentPadding = PaddingValues(65.dp),
+        modifier = Modifier.fillMaxSize(),
     ) {
         items(movies) { movie ->
             MovieItemView(movie = movie)
         }
     }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun MoviesGrid() {
+    AllMoviesView(MainPageViewModel().premieres)
 }
