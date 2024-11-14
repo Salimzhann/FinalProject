7c7f3996107d740d1dc6e4ebf5c561b9aa05c6b6
Zhandos04 <nurbekulyzhandos@gmail.com>
2024-11-14 17:04:48 +0500

    StaffCard updated

--- app/src/main/java/com/example/finalproject/ui/screens/StaffCard.kt
+++ app/src/main/java/com/example/finalproject/ui/screens/StaffCard.kt
@@ -3,35 +3,42 @@ package com.example.finalproject.ui.screens
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import coil.compose.rememberAsyncImagePainter
 import com.example.finalproject.domain.model.StaffMember
 
 @Composable
 fun StaffCard(staff: StaffMember, onClick: () -> Unit) {
-    Card(
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
         modifier = Modifier
-            .padding(8.dp)
-            .width(150.dp)
             .clickable(onClick = onClick)
+            .padding(start = 16.dp, top = 8.dp, bottom = 8.dp)
     ) {
-        Column(horizontalAlignment = Alignment.CenterHorizontally) {
-            Image(
-                painter = rememberAsyncImagePainter(model = staff.posterUrl),
-                contentDescription = "Staff Image",
-                modifier = Modifier.size(100.dp)
-            )
-            Text(staff.nameRu, style = MaterialTheme.typography.bodyMedium)
-            Text(staff.professionText, style = MaterialTheme.typography.bodyLarge)
+        Image(
+            painter = rememberAsyncImagePainter(model = staff.posterUrl),
+            contentDescription = "Staff Image",
+            modifier = Modifier
+                .size(70.dp, 100.dp)
+                .clip(RoundedCornerShape(10.dp))
+        )
+        Column {
+            Text(staff.nameRu, style = MaterialTheme.typography.bodyLarge)
+            Text(staff.professionText, style = MaterialTheme.typography.bodyMedium, color = Color.Gray)
         }
     }
 }
+
